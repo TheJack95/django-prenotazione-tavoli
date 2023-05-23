17b81cf22357fb012ae9daae31b96670897f3446
Giacomo <giacomocorro@gmail.com>
2023-05-23 21:03:58 +0200

    first commit: creata app django

--- PrenotazionetìTavoli/PrenotazionetìTavoli/asgi.py
+++ PrenotazionetìTavoli/PrenotazionetìTavoli/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangoPrenotazioneTavoli project.
+ASGI config for PrenotazionetìTavoli project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoPrenotazioneTavoli.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PrenotazionetìTavoli.settings')
 
 application = get_asgi_application()
--- PrenotazionetìTavoli/PrenotazionetìTavoli/wsgi.py
+++ PrenotazionetìTavoli/PrenotazionetìTavoli/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangoPrenotazioneTavoli project.
+WSGI config for PrenotazionetìTavoli project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoPrenotazioneTavoli.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PrenotazionetìTavoli.settings')
 
 application = get_wsgi_application()
